f1922f818eba72cf7c163bc124d2616a727fe46f
JennyT-CP <jentipan7@gmail.com>
2020-03-03 17:59:09 -0500

    2.0

--- appFulltime/src/app/componentes/empleado/registro/registro.component.ts
+++ appFulltime/src/app/componentes/empleado/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { EmpleadoService } from 'src/app/servicios/empleado/empleado.service';
 
 @Component({
@@ -10,9 +10,10 @@ import { EmpleadoService } from 'src/app/servicios/empleado/empleado.service';
 export class RegistroComponent implements OnInit {
 
   public nuevoEmpleadoForm = new FormGroup({
-    nombreForm: new FormControl('', [Validators.required,Validators.pattern('[a-zA-z]+')]),
+
+    nombreForm: new FormControl('', [Validators.required, Validators.pattern('[a-zA-z]+')]),
     apellidoForm: new FormControl('', [Validators.required, Validators.pattern('[a-zA-z]+')]),
-    cedulaForm: new FormControl('', [Validators.required, Validators.pattern('[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]') ]),
+    cedulaForm: new FormControl('', [Validators.required, Validators.pattern('[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'), Validators.maxLength(10)]),
     emailForm: new FormControl('', [Validators.required, Validators.email]),
     telefonoForm: new FormControl('', Validators.required),
     domicilioForm: new FormControl('', Validators.required),
@@ -20,7 +21,7 @@ export class RegistroComponent implements OnInit {
     estadoCivilForm: new FormControl('', Validators.required),
     generoForm: new FormControl('', Validators.required),
     estadoForm: new FormControl('', Validators.required),
-    correoAlternativoForm: new FormControl('', [Validators.email,Validators.required]),
+    correoAlternativoForm: new FormControl('', [Validators.email, Validators.required]),
   });
   constructor(
     public rest: EmpleadoService
@@ -30,7 +31,26 @@ export class RegistroComponent implements OnInit {
     this.limpliarCampos();
   }
 
-  limpliarCampos(){
+  soloNumeros(e) {
+    let key = e.keyCode || e.which;
+    let tecla = String.fromCharCode(key).toLowerCase();
+    let numeros = "0123456789";
+    let especiales = [8, 37, 39, 46];
+
+    let tecla_especial = false
+    for (var i in especiales) {
+      if (key == especiales[i]) {
+        tecla_especial = true;
+        break;
+      }
+    }
+
+    if (numeros.indexOf(tecla) == -1 && !tecla_especial)
+      return false;
+  }
+
+  limpliarCampos() {
+
     this.nuevoEmpleadoForm.setValue({
       nombreForm: '',
       apellidoForm: '',
@@ -46,7 +66,9 @@ export class RegistroComponent implements OnInit {
     });
   }
 
-  insertarEmpleado(form){
+ 
+
+  insertarEmpleado(form) {
     let dataEmpleado = {
       cedula: form.cedulaForm,
       apellido: form.apellidoForm,
@@ -62,13 +84,13 @@ export class RegistroComponent implements OnInit {
     };
 
     this.rest.postEmpleadoRest(dataEmpleado)
-    .subscribe(
-      response => {
-        console.log(response);
-      },
-      error => {
-        console.log(error);
-      });
+      .subscribe(
+        response => {
+          console.log(response);
+        },
+        error => {
+          console.log(error);
+        });
 
     this.limpliarCampos();
 
