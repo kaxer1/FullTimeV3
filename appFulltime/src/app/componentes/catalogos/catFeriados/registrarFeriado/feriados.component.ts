fed50ed9c1427c96a8aa03832e41986468fab7e4
JennyT-CP <jentipan7@gmail.com>
2020-03-09 17:51:21 -0500

    2.7

--- appFulltime/src/app/componentes/catalogos/catFeriados/registrarFeriado/feriados.component.ts
+++ appFulltime/src/app/componentes/catalogos/catFeriados/registrarFeriado/feriados.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import {MatDialog} from '@angular/material/dialog';
-import {FeriadosService } from 'src/app/servicios/catalogos/feriados/feriados.service';
+import {FeriadosService } from '../../../../servicios/catalogos/feriados/feriados.service';
 import { RelojesComponent } from 'src/app/componentes/catalogos/relojes/relojes.component';
 
 @Component({
