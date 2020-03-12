ff081792342447a4e58a21fec851f18a77f10fbf
kaxer1 <kevincuray41@gmail.com>
2020-03-12 15:58:46 -0500

    correcciones pantallas

--- appFulltime/src/app/componentes/catalogos/horas-extras/horas-extras.component.ts
+++ appFulltime/src/app/componentes/catalogos/horas-extras/horas-extras.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators, FormGroup } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { HorasExtrasService } from 'src/app/servicios/catalogos/horas-extras.service';
 
@@ -27,20 +27,6 @@ export class HorasExtrasComponent implements OnInit {
   tipoFuncion = new FormControl('', Validators.required);
   diaHoraExtra = new FormControl('', Validators.required);
 
-  public nuevaHoraExtraForm = new FormGroup({
-    descripcionForm: this.descripcion,
-    tipoDescuentoForm: this.tipoDescuento,
-    recaPorcentajeForm: this.recaPorcentaje,
-    diaHoraExtraForm: this.diaHoraExtra, //campo extra para poder almacenar el dia
-    horaInicioForm: this.horaInicio,
-    horaFinalForm: this.horaFinal,
-    horaJornadaForm: this.horaJornada,
-    tipoDiaForm: this.tipoDia,
-    codigoForm: this.codigo,
-    inclAlmuerzoForm: this.inclAlmuerzo,
-    tipoFuncionForm: this.tipoFuncion
-  });
-
   descuentos: TipoDescuentos[] = [
     {value: '1', viewValue: 'desc1'},
     {value: '2', viewValue: 'desc2'},
@@ -48,33 +34,53 @@ export class HorasExtrasComponent implements OnInit {
     {value: '4', viewValue: 'desc4'},
   ];
 
+  isLinear = true;
+  primeroFormGroup: FormGroup;
+  segundoFormGroup: FormGroup;
+
   constructor(
     private toastr: ToastrService,
-    private rest: HorasExtrasService
+    private rest: HorasExtrasService,
+    private _formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
     this.limpiarCampos();
+    this.primeroFormGroup = this._formBuilder.group({
+      descripcionForm: this.descripcion,
+      tipoDescuentoForm: this.tipoDescuento,
+      recaPorcentajeForm: this.recaPorcentaje,
+      diaHoraExtraForm: this.diaHoraExtra, //campo extra para poder almacenar el dia
+      horaInicioForm: this.horaInicio,
+      horaFinalForm: this.horaFinal
+    });
+    this.segundoFormGroup = this._formBuilder.group({
+      horaJornadaForm: this.horaJornada,
+      tipoDiaForm: this.tipoDia,
+      codigoForm: this.codigo,
+      inclAlmuerzoForm: this.inclAlmuerzo,
+      tipoFuncionForm: this.tipoFuncion
+    });
   }
 
   formatLabel(value: number) {
     return value + '%';
   }
 
-  insertarHoraExtra(form) {
-    let fechaHoraInicio = form.diaHoraExtraForm + ' ' + form.horaInicioForm + ':00';
-    let fechaHoraFin = form.diaHoraExtraForm + ' ' + form.horaFinalForm + ':00';
+  insertarHoraExtra(form1, form2) {
+    let fechaHoraInicio = form1.diaHoraExtraForm + ' ' + form1.horaInicioForm + ':00';
+    let fechaHoraFin = form1.diaHoraExtraForm + ' ' + form1.horaFinalForm + ':00';
     let dataHoraExtra = {
-      descripcion: form.descripcionForm,
-      tipo_descuento: form.tipoDescuentoForm,
-      reca_porcentaje: form.recaPorcentajeForm,
+      descripcion: form1.descripcionForm,
+      tipo_descuento: form1.tipoDescuentoForm,
+      reca_porcentaje: form1.recaPorcentajeForm,
       hora_inicio: fechaHoraInicio,
       hora_final: fechaHoraFin,
-      hora_jornada: form.horaJornadaForm,
-      tipo_dia: form.tipoDiaForm,
-      codigo: form.codigoForm,
-      incl_almuerzo: form.inclAlmuerzoForm,
-      tipo_funcion: form.tipoFuncionForm
+      hora_jornada: form2.horaJornadaForm,
+      tipo_dia: form2.tipoDiaForm,
+      codigo: form2.codigoForm,
+      incl_almuerzo: form2.inclAlmuerzoForm,
+      tipo_funcion: form2.tipoFuncionForm
     };
     console.log(dataHoraExtra);
 
@@ -82,14 +88,14 @@ export class HorasExtrasComponent implements OnInit {
       .subscribe(response => {
         this.toastr.success('Operación Exitosa', 'Hora extra guardada');
         // this.limpiarCampos();
-        this.nuevaHoraExtraForm.reset();
+        // this.nuevaHoraExtraForm.reset();
       }, error => {
         console.log(error);
       });;
   }
 
   limpiarCampos() {
-    this.nuevaHoraExtraForm.reset();
+    // this.nuevaHoraExtraForm.reset();
   }
 
 }
