9753a6b6751f6ac7c7f08dd8f7bf815559860936
JennyT-CP <jentipan7@gmail.com>
2020-04-13 12:32:10 -0500

    4.10 Arreglos Iconos Pantallas

--- appFulltime/src/app/share/main-nav/main-nav.component.ts
+++ appFulltime/src/app/share/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, HostBinding, Input, OnInit } from '@angular/core';
+import { Component, ViewChild, HostBinding, Input, OnInit,  } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css'],
+  styleUrls: ['./main-nav.component.css']
 
 })
 export class MainNavComponent {
