991e2975c1c81ae9cad16f374402479655aabb11
JennyT-CP <jentipan7@gmail.com>
2020-04-27 09:49:50 -0500

    5.10 Unificacion de codigo

--- servidor/src/database.ts
+++ servidor/src/database.ts
@@ -7,7 +7,7 @@ const pool = new Pool({
   host: 'localhost',
   port: 5432,  
   database: 'fulltime3',
-  password: 'fulltime',
+  password: 'admin',
 
 })
 pool.query('SELECT NOW()', (err, res) => {
