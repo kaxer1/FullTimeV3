481878af36267bf2f9b431d7226743440f262bbb
kaxer1 <kevincuray41@gmail.com>
2021-06-03 18:27:50 -0500

    sp26

--- servidor/src/database.ts
+++ servidor/src/database.ts
@@ -6,12 +6,13 @@ const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   port: 5432,
-  // database: 'Fulltime1504',
-  database: 'FulltimeModulos',
+  database: 'Fulltime1504',
+  // database: 'FulltimeModulos',
   // database: 'FulltimePresentaion',
   // database: 'FulltimePruebaMigracion',
   password: 'fulltime'
 })
+
 pool.query('SELECT NOW()', (err, res) => {
   if (err) {
     console.log("Error durante la conexión", err)
