231a99152fd29fbcc41f5919cc54d2f8656da87f
JennyT-CP <jentipan7@gmail.com>
2021-06-03 19:13:22 -0500

    actualizar-03-06-2021-03

--- servidor/javascript/libs/settingsMail.js
+++ servidor/javascript/libs/settingsMail.js
@@ -12,7 +12,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.enviarMail = exports.Credenciales = exports.email = void 0;
 const nodemailer_1 = __importDefault(require("nodemailer"));
 const database_1 = __importDefault(require("../database"));
 exports.email = process.env.EMAIL || '';
